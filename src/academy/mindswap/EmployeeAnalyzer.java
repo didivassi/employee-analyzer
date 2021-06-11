be590dc14fc3065e903ca63376491abb4c787e58
didivassi <diogovelho@gmail.com>
2021-06-11 16:28:12 +0100

    Initial commit

---
+++ src/academy/mindswap/EmployeeAnalyzer.java
@@ -0,0 +1,62 @@
+package academy.mindswap;
+
+import academy.mindswap.employee.Employee;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class EmployeeAnalyzer {
+
+    public int countEmployeesByWorkAge(List<Employee> department, int tillYear, int numberOfYears){
+        return (int) department
+                .stream()
+                .filter(employee -> tillYear-employee.getStartingYear()>= numberOfYears)
+                .count();
+    }
+
+    public List<String> getEmployeesNameBySalaryAboveOrEqual(List<Employee> department, int salaryFrom){
+        return department
+                .stream()
+                .filter(employee -> employee.getSalary()>= salaryFrom)
+                .map(employee ->  employee.getFirstName()  + " " + employee.getLastname())
+                .collect(Collectors.toList());
+    }
+
+    public List<Employee> getOldestNEmployees(List<Employee> department, int n){
+        return department
+                .stream()
+                .sorted(Comparator.comparingInt(Employee::getAge).reversed())
+                .limit(n)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<Employee> getFirstEmployeeOlderThan(List<Employee> department, int age){
+       return department
+               .stream()
+               //.sorted(Comparator.comparingInt(Employee::getAge))
+               .filter(employee -> employee.getAge()>= age)
+               .findFirst();
+    }
+
+    public double getAverageSalary(List<Employee> department){
+        return department
+                .stream()
+                .mapToDouble(Employee::getSalary)
+                .average()
+                .orElse(Double.NaN);
+    }
+
+    public List<String> findCommonNamesInDepartments(List<Employee> department, List<Employee> department2){
+        List<String> department2Names=department2.stream().map(Employee::getFirstName).collect(Collectors.toList());
+        return department
+                .stream()
+                .map(Employee::getFirstName)
+                .filter(employee ->  department2Names.contains(employee))
+                .distinct()
+                .collect(Collectors.toList());
+
+    }
+
+}
---
+++ src/academy/mindswap/Main.java
@@ -0,0 +1,24 @@
+package academy.mindswap;
+
+import academy.mindswap.db.DB;
+import academy.mindswap.employee.Employee;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EmployeeAnalyzer analyzer = new EmployeeAnalyzer();
+        //7
+        System.out.println(analyzer.countEmployeesByWorkAge(DB.getHrDepartment(), 2021, 4));
+        //Reina Hopkins
+        System.out.println(analyzer.getEmployeesNameBySalaryAboveOrEqual(DB.getHrDepartment(), 1500));
+        //Reina,Brian,Frank
+        System.out.println(analyzer.getOldestNEmployees(DB.getHrDepartment(), 3));
+        //Edna Heller
+        System.out.println(analyzer.getFirstEmployeeOlderThan(DB.getHrDepartment(), 24));
+        //Mary,James
+        System.out.println(analyzer.findCommonNamesInDepartments(DB.getHrDepartment(), DB.getSalesDepartment()));
+    }
+}
