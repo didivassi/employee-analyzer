31836dda007c19863c1b9da7bf48a5dc02b76e21
didivassi <diogovelho@gmail.com>
2021-06-11 16:38:32 +0100

    added comment to average

--- src/academy/mindswap/Main.java
+++ src/academy/mindswap/Main.java
@@ -19,7 +19,7 @@ public class Main {
         System.out.println(analyzer.getOldestNEmployees(DB.getHrDepartment(), 3));
         //Edna Heller
         System.out.println(analyzer.getFirstEmployeeOlderThan(DB.getHrDepartment(), 24));
-
+        //1327.7777777777778
         System.out.println(analyzer.getAverageSalary(DB.getHrDepartment()));
         //Mary,James
         System.out.println(analyzer.findCommonNamesInDepartments(DB.getHrDepartment(), DB.getSalesDepartment()));
