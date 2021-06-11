28807c6830161c45abb5ff143f5b1c6d2c6de246
didivassi <diogovelho@gmail.com>
2021-06-11 16:52:17 +0100

    changedcomment

--- src/academy/mindswap/Main.java
+++ src/academy/mindswap/Main.java
@@ -21,7 +21,7 @@ public class Main {
         System.out.println(analyzer.getFirstEmployeeOlderThan(DB.getHrDepartment(), 24));
         //1327.7777777777778
         System.out.println(analyzer.getAverageSalary(DB.getHrDepartment()));
-        //Mary,James
+        //James, Mary
         System.out.println(analyzer.findCommonNamesInDepartments(DB.getHrDepartment(), DB.getSalesDepartment()));
     }
 }
